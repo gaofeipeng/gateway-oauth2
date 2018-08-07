2e10bb298c1e8febccc185ddb0792b52612cd9bd
xiaosheng <945756181@qq.com>
2018-08-07 22:16:11 +0800

    An error occurred when the service was started

---
+++ src/main/java/com/gatewayoauth2/demo/GatewayOauth2Application.java
@@ -0,0 +1,12 @@
+package com.gatewayoauth2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayOauth2Application.class, args);
+    }
+}
